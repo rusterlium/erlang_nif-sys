e62f84167dca6229018aabd7db4413ec2c26717a
Daniel Goertzen <daniel.goertzen@gmail.com>
2017-03-03 12:06:54 -0600

    (cargo-release) generate docs

---
+++ implementors/rand/distributions/trait.IndependentSample.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["erlang_nif_sys"] = [];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
